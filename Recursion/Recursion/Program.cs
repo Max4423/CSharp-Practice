94d57049329b2f60850e8e0588b69de6dd8e305e
Максим Гладун <123461058+Max4423@users.noreply.github.com>
2025-01-24 01:17:25 +0200

    23.01

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Recursion
+{
+	internal class Program
+	{
+		static void Foo(int i)
+		{
+			Console.WriteLine(i);
+			i++;
+			if (i <= 15000)
+			{
+				Foo(i);
+
+			}
+
+		}
+
+		static void PrintArr(int[] arr, int i = 0)
+		{
+			if (i >= arr.Length)
+			{
+				return;
+			}
+			Console.Write(arr[i] + " ");
+
+			PrintArr(arr, ++i);
+
+		}
+
+		static int SumArr(int[] arr, int i = 0)
+		{
+			if (arr.Length == i)
+			{
+				return 0;
+			}
+
+			return SumArr(arr, i + 1) + arr[i];
+		}
+		static void Main(string[] args)
+		{
+			int[] arr = { 1, 4, 5, 2, 4, 22, 1, 0, 45 };
+
+			PrintArr(arr);
+			Console.WriteLine();
+			Console.WriteLine(SumArr(arr));
+		}
+	}
+}
---
+++ sumOfDigits/sumOfDigits/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace sumOfDigits
+{
+    internal class Program
+    {
+		static int SumOfDigits(int value)
+        {
+            if (value == 0)
+            {
+                return 0;
+            }
+
+            return SumOfDigits(value / 10) + value % 10;
+        }
+
+		static int SumOfDigitsLoop(int value)
+		{
+            int sum = 0;
+
+			while (value != 0)
+            {
+                sum += value % 10;
+                value /= 10;
+            }
+            return sum;
+		}
+
+		static void Main(string[] args)
+        {
+            int value = 234;
+
+            int result = SumOfDigits(value);
+			Console.WriteLine(result);
+			Console.WriteLine(SumOfDigitsLoop(value));
+        }
+    }
+}
