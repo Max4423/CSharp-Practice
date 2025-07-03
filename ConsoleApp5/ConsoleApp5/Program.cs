72ba523165f7cc94fbda8a647b6107bd41dc9ca3
Максим Гладун <123461058+Max4423@users.noreply.github.com>
2025-07-04 00:27:17 +0300

    03.07

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ConsoleApp5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			a aa = new();
+
+			System.Console.WriteLine(aa.GetType().IsClass);
+		}
+	}
+
+	public class a { int x; }
+
+}
