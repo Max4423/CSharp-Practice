5e969204be6a6daa72dd538d16fc30f158cf491c
Максим Гладун <123461058+Max4423@users.noreply.github.com>
2025-11-07 13:39:43 +0200

    07.11 - 03

---
+++ Codewars - 1/ConsoleApp6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            long[] numbers = Digitizer.Digitize(123456789);
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+
+    class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            int NumbersCount = n.ToString().Length;
+
+            if (n == 0)
+            {
+                return new long[] { 0 };
+            }
+
+            var digits = new List<long>();
+
+            long tempNumber = n;
+
+            while (tempNumber > 0)
+            {
+                digits.Add(tempNumber % 10);
+                tempNumber /= 10;
+            }
+
+            return digits.ToArray();
+        }
+    }
+}
