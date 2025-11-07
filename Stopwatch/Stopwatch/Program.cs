8cfba3db1cc281af314156f4d3f8233f0bc13c55
Максим Гладун <123461058+Max4423@users.noreply.github.com>
2025-11-07 13:34:28 +0200

    07.11

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Stopwatch
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var timer = new System.Diagnostics.Stopwatch();
+
+			timer.Start();
+
+			int[] arr = new int[1_000_000];
+			for (int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = i * 2;
+			}
+			Console.WriteLine(timer.ElapsedTicks);
+			timer.Restart();
+			int[] arr2 = new int[100_000];
+			for (int i = 0; i < arr2.Length; i++)
+			{
+				arr2[i] = i * 2;
+			}  
+			Console.WriteLine(timer.ElapsedTicks);
+		}
+	}
+}
---
+++ WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+
+namespace WorkingWithStrings
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string s1 = "str";
+
+			string s2 = new string(new char[] { 's', 't', 'r' });
+
+			string s3 = new string('a', 5); // "aaaaa"
+
+			string s4 = new string(new char[] { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd' }, 6, 5); // "world"
+
+			Console.WriteLine(s1[1]);
+			Console.WriteLine(s1.Length);
+
+			Console.WriteLine("-------------");
+			foreach (char c in s1)
+			{
+				Console.WriteLine(c);
+			}
+
+			for (int i = 0; i < s1.Length; i++)
+			{
+				Console.WriteLine(s1[i]);
+			}
+
+			string message1 = "hello";
+			string message2 = "hello";
+
+			Console.WriteLine(message1 == message2);
+
+			var ss1 = s1.Insert(3, "ks");
+
+			Console.WriteLine(ss1);
+
+			var split = ss1.Split("r");
+
+			foreach (var item in split)
+			{
+				Console.WriteLine(item);
+			}
+
+			var sub = s3.Substring(0, 1);
+
+			Console.WriteLine(s4.ToUpper());
+
+			Console.WriteLine(new string("      cwd cd c          ").Trim());
+
+			Console.WriteLine(message1.IndexOf("l"));
+
+			string[] toJoin = new string[] { "cweferfq", "dveverq", "ccfdvq" };
+			string.Concat("hello", " ", "world");
+
+			Console.WriteLine(string.Join(", ", toJoin));
+
+			Console.WriteLine(string.Compare(message1, message2));// == 0 - Equal
+
+			Console.WriteLine(string.Compare(message1, "abc"));
+
+			Console.WriteLine(message1.IndexOf("hell"));
+
+
+
+			StringBuilder sb = new StringBuilder("Hello world!");
+			Console.WriteLine($"Length: {sb.Length}");
+			Console.WriteLine($"Capacity: {sb.Capacity}");
+
+			sb.Append(" Welcome to C# programming.");
+			Console.WriteLine(sb);
+			Console.WriteLine($"Length: {sb.Length}");
+			Console.WriteLine($"Capacity: {sb.Capacity}");
+
+			sb.Append(" Hello!");
+			Console.WriteLine(sb);
+			Console.WriteLine($"Length: {sb.Length}");
+			Console.WriteLine($"Capacity: {sb.Capacity}");
+
+			sb.Append(" Ku!");
+			Console.WriteLine(sb);
+			Console.WriteLine($"Length: {sb.Length}");
+			Console.WriteLine($"Capacity: {sb.Capacity}");
+
+			sb.Insert(7, " qwerty!");
+			Console.WriteLine(sb);
+
+			sb.Replace("Hello", "KUKU");
+			Console.WriteLine(sb);
+
+			sb.Remove(0,20);
+			Console.WriteLine(sb);
+
+			string finalString = sb.ToString();
+
+		}
+	}
+}
