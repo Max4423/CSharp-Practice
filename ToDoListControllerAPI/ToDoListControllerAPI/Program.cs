3e7af849c8591293fe4326533a658c00c611f805
Максим Гладун <123461058+Max4423@users.noreply.github.com>
2026-01-11 19:31:09 +0200

    11.01

---
+++ ToDoListControllerAPI/ToDoListControllerAPI/Program.cs
@@ -0,0 +1,35 @@
+using ToDoListControllerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+	config.DocumentName = "TodoAPI";
+	config.Title = "TodoAPI v1";
+	config.Version = "v1";
+});
+builder.Services.AddDbContext<ToDoContext>(opt => opt.UseInMemoryDatabase("ToDoList"));
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseOpenApi();
+	app.UseSwaggerUi(config =>
+	{
+		config.DocumentTitle = "TodoAPI";
+		config.Path = "/swagger";
+		config.DocumentPath = "/swagger/{documentName}/swagger.json";
+		config.DocExpansion = "list";
+	});
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
